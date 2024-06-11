deda33ddb60f3187b6f601d6af519766b373823b
djokodev <djokodev75@gmail.com>
2024-06-11 01:27:12 +0100

    endpoint api/product implemented for list all products

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework.serializers import ModelSerializer
-from shop.models import Category
+from shop.models import Category, Product
  
 class CategorySerializer(ModelSerializer):
- 
     class Meta:
         model = Category
         fields = ['id', 'name']
+
+
+class ProductSerializer(ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'category', 'date_created', 'date_updated']
--- shop/views.py
+++ shop/views.py
@@ -1,11 +1,20 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from shop.models import Category
-from shop.serializers import CategorySerializer
+from shop.models import Category, Product
+from shop.serializers import CategorySerializer, ProductSerializer
  
 class CategoryAPIView(APIView):
- 
+
     def get(self, *args, **kwargs):
         categories = Category.objects.all()
         serializer = CategorySerializer(categories, many=True)
         return Response(serializer.data)
+    
+
+class ProductAPIView(APIView):
+
+    def get(self, *args, **kwargs):
+        queryset = Product.objects.all()
+        serializer = ProductSerializer(queryset, many=True)
+        return Response(serializer.data)
+    
