bc5aec878f1c8583d7ecdbac66d2967ff742e515
djokodev <djokodev75@gmail.com>
2024-06-11 01:50:46 +0100

    [feat]: viewset implementation in the shop application for product view and router configuration

--- project/urls.py
+++ project/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-from shop.views import CategoryViewset, ProductAPIView
+from shop.views import CategoryViewset, ProductViewset
 from rest_framework import routers
 
 from shop.views import CategoryViewset
 
 router = routers.SimpleRouter()
 router.register('category', CategoryViewset, basename='category')
+router.register('product', ProductViewset, basename='product')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- shop/views.py
+++ shop/views.py
@@ -1,19 +1,17 @@
 from rest_framework.views import APIView
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ReadOnlyModelViewSet
 from rest_framework.response import Response
 from shop.models import Category, Product
 from shop.serializers import CategorySerializer, ProductSerializer
  
-class CategoryViewset(ModelViewSet):
+class CategoryViewset(ReadOnlyModelViewSet):
     serializer_class = CategorySerializer
 
     def get_queryset(self):
         return Category.objects.all()
 
-class ProductAPIView(APIView):
+class ProductViewset(ReadOnlyModelViewSet):
+    serializer_class = ProductSerializer
 
-    def get(self, *args, **kwargs):
-        queryset = Product.objects.all()
-        serializer = ProductSerializer(queryset, many=True)
-        return Response(serializer.data)
-    
+    def get_queryset(self):
+        return Product.objects.all()
